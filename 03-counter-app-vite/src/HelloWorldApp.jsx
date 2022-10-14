6196781d92a28090099adabe575887520c32fa73
GisbelT <gisbeltq@gmail.com>
2022-10-14 16:54:19 -0400

    hello world in React

--- 03-counter-app-vite/src/HelloWorldApp.jsx
+++ 03-counter-app-vite/src/HelloWorldApp.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 //we call the App component in main.jsx
 const App = () => {
-  return <h1>Hello World</h1>;  
+  return <h1> Hello World </h1>;  
 }
 
 export default App
