53d5d77ba925adff65db6f8215e1a6fb035b7109
GisbelT <gisbeltq@gmail.com>
2022-10-14 17:01:06 -0400

    counter app useState

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -26,9 +26,9 @@ const CounterApp = ({ value }) => {
         <>
         <h1>Counter Here!</h1>
         <h2>{ counter }</h2>
-        <button onClick={handleAdd}>+1</button>
-        <button onClick={handleSubtract}>-1</button>
-        <button onClick={handleReset}>Reset</button>
+        <button onClick={ handleAdd }>+1</button>
+        <button onClick={ handleSubtract }>-1</button>
+        <button onClick={ handleReset }>Reset</button>
         </>
     )
 }
