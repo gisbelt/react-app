05033010f886885991fbded8a231647c41083bd2
GisbelT <gisbeltq@gmail.com>
2022-10-24 23:31:04 -0400

    testing file 11-async-await - jest-config - jest-setup - whatwg-fetch

--- 03-counter-app-vite/src/test-bases/11-async-await.js
+++ 03-counter-app-vite/src/test-bases/11-async-await.js
@@ -1,24 +1,18 @@
 
-
-// const getImagenPromesa = () => new Promise( resolve => resolve('https://ajskdhaskjdhajs.com') )
-// getImagenPromesa().then( console.log );
-
-const getImage = async() => {
+export const getImage = async() => {
     try {
-        const apiKey = '9O1lQTPPrrsDfbowNS3VKp2CrO6q1bL8';
+        const apiKey = '8j8vGOFVc1stPezcogfXyRngWHCv7syU';
         const http_call = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
         const {data} = await http_call.json();
         const {url} = data.images.original;
-        // console.log(url)
-        const img = document.createElement('img');
-        img.src = url;
-        document.body.append(img)
+
+        return url;
     } catch (error) {
         console.error(error)
+        return 'Image not found';
     }
     
 }
-getImage()
 
 
 
