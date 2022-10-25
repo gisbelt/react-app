1f67e40bcf73c87da0296d9eb0f0546ad33ab0c7
GisbelT <gisbeltq@gmail.com>
2022-10-24 20:27:24 -0400

    testing file 05-functions - toEqual

---
+++ 03-counter-app-vite/src/test-bases/05-functions.js
@@ -0,0 +1,15 @@
+export const getUser = () => ({
+    uid: 'ABC123',
+    username: 'Baby_1501'
+});
+
+
+//Practice
+export const getUserActive = (names) => ({
+    uid: 'ABC567',
+    username: names
+});
+
+
+
+
