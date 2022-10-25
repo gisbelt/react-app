01ae286000815a47ef6a4246f3e6b543d7f13904
GisbelT <gisbeltq@gmail.com>
2022-10-24 20:43:37 -0400

    testing file 06-deses-obj - toEqual

--- 03-counter-app-vite/src/test-bases/06-deses-obj.js
+++ 03-counter-app-vite/src/test-bases/06-deses-obj.js
@@ -1,19 +1,6 @@
 
-//Destructuring of objects
-const person = {
-    name: 'Tony',
-    age: 45,
-    password: 'Stark123'
-};
-
-// const { edad, clave, nombre, } = persona;
- 
-// console.log( nombre );
-// console.log( edad );
-// console.log( clave );
-
 //Extract object from object
-const userContext2 = ({name, age, password, range = "Captain"}) =>{
+export const userContext2 = ({name, age, password, range = "Captain"}) =>{
     return {
         passName: password,
         year: age,
@@ -23,21 +10,3 @@ const userContext2 = ({name, age, password, range = "Captain"}) =>{
         }
     }
 }
-
-// latlng:{lat,lng} extract lat and lng from latlng, print lat and lng
-const avenger2 =  ({passName,year, latlng:{lat,lng}}) =>{
-    console.log(passName, year, lat, lng);
-}
-avenger2(userContext2(person));
-
-//Another way
-const avenger3 =  ({passName,year,latlng}) =>{
-    console.log(passName, year);
-    // Destructuring object latlng from userContext2 
-    const printlat = ({lat, lng}) => {
-        console.log(lat, lng);
-    }
-    printlat(latlng);
-}
-avenger3(userContext2(person));
-
