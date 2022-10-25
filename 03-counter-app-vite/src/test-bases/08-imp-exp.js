086845112514622fcf70c5b6e53259b9d282ce89
GisbelT <gisbeltq@gmail.com>
2022-10-24 22:06:05 -0400

    testing file 08-imp-exp - toBeFalsy - toEqual

--- 03-counter-app-vite/src/test-bases/08-imp-exp.js
+++ 03-counter-app-vite/src/test-bases/08-imp-exp.js
@@ -1,19 +1,7 @@
-// import { heroes } from './data/heroes';
-// import { heroes } from './data/heroes';
-// import heroes, { owners } from '../data/heroes';
 import heroes from '../data/heroes';
-
-// console.log( owners );
-
 
 /*************FIND****************/
 export const getHeroeById = (id) => heroes.find( (heroe) => heroe.id === id );
-console.log(getHeroeById(2));
-
-export const getHeroesByOwner = ( owner ) => heroes.filter( (heroe) => heroe.owner === owner );
-onsole.log(getHeroeByOwner('DC'));
 
 /*************FILTER****************/
-//Filter() all heroes that owner is 'DC'
-const getHeroeByOwner2 = (owner) => heroes.filter ((heroe) => heroe.owner === owner)
-console.log(getHeroeByOwner2('DC'));
+export const getHeroeByOwner = ( owner ) => heroes.filter ( (heroe) => heroe.owner === owner );
