b6e888efe1cd376f19056367c7e93708c3e1d53e
GisbelT <gisbeltq@gmail.com>
2022-10-24 21:25:16 -0400

    testing file 07-deses-arr

--- 03-counter-app-vite/src/test-bases/07-deses-arr.js
+++ 03-counter-app-vite/src/test-bases/07-deses-arr.js
@@ -1,25 +1,8 @@
-//Destructuring of arrays
-const caracters = ['Goku', 'Vegeta', 'Trunks'];
-// if we want the second element from array 'caracters', we put a , before 
-const [, p2] = caracters;
-console.log(p2)
-
 //Extracting the array from function returnArray
-const returnArray = () =>{
+export const returnArray = () =>{
     return ['ABC', 123];
 }
-const [letters, numbers] = returnArray();
-console.log(letters, numbers);
-
-//Practice
-//Extracting  the name and the function
-//Remember that useeState return a array with 2 items and the second item is a function 
-const useeState = (value) =>{
-    return [value, () => {console.log('Hola mundo')}];
-}
-const [names, setName] = useeState('Goku');
-console.log(names);
-setName();
+// const [letters, numbers] = returnArray();
 
 
 
