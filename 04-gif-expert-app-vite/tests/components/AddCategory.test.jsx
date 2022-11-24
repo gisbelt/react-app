ed16b5667990a8f2fe78e48df2116fac6056176a
GisbelT <gisbeltq@gmail.com>
2022-11-23 22:30:39 -0400

    testing AddCategory - jest.fn() - fireEvent: input, submit - screen: getByRole - toHaveBeenCalled

--- 04-gif-expert-app-vite/tests/components/AddCategory.test.jsx
+++ 04-gif-expert-app-vite/tests/components/AddCategory.test.jsx
@@ -15,9 +15,9 @@ describe('Testing component <AddCategory />', () => {
 
     test('should call onNewValue if input have a value', () => { 
         const inputValue = 'DragonBall'
-        //TODO: ???
+        const onNewValue = jest.fn(); //function
 
-        render( <AddCategory onNewValue={ () => {} } /> );
+        render( <AddCategory onNewValue={ onNewValue } /> );
         const input = screen.getByRole('textbox');
         //form: is reading it from the "aria-label" in the component 
         const form = screen.getByRole("form");
@@ -27,5 +27,21 @@ describe('Testing component <AddCategory />', () => {
         fireEvent.submit( form );
         //expect input empty
         expect( input.value ).toBe("")
+        //function is expected to have been called once
+        expect( onNewValue ).toHaveBeenCalledTimes(1)
+        //function is expected to have been called with value DragonBall
+        expect( onNewValue ).toHaveBeenCalledWith( inputValue )
+    })
+
+    test('should not call onNewValue if input is empty', () => { 
+        const onNewValue = jest.fn(); //function
+        render( <AddCategory onNewValue={ onNewValue } /> );
+        //form: is reading it from the "aria-label" in the component 
+        const form = screen.getByRole("form");
+        //trigger event submit
+        fireEvent.submit( form );
+        //function is expected to haven't been called 
+        expect( onNewValue ).toHaveBeenCalledTimes(0)
+        expect( onNewValue ).not.toHaveBeenCalled()
     })
 })
