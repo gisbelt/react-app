34b222544905a237af33a00078596f1c7ece714c
GisbelT <gisbeltq@gmail.com>
2022-12-02 19:56:00 -0400

    form with custom hook: useForm

---
+++ 05-hook-app/src/02-useEffect/FormWithCustomHook.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+import { useForm } from '../hooks/useForm';
+import { Message } from './Message';
+
+export const FormWithCustomHook = () => {
+
+    //Custom Hook
+    const { formState, onInputChange, username, email, password  } = useForm({
+        username: '',
+        email: '',
+        password: ''
+    })
+    // const {username, email, password } = formState;
+    
+    return (
+        <>
+            <h1>Form With Custom Hook</h1>
+            <hr />
+            <div className="mb-3">
+                <label htmlFor="username" className="form-label">Username:</label>
+                <input 
+                    type="text"
+                    className="form-control" 
+                    name="username" 
+                    placeholder="Username:"
+                    value={ username }
+                    onChange={ onInputChange }
+                />
+                <input 
+                    type="email"
+                    className="form-control mt-2" 
+                    name="email" 
+                    placeholder="gis@gmail.com"
+                    value={ email }
+                    onChange={ onInputChange }
+                />
+                <input 
+                    type="password"
+                    className="form-control mt-2" 
+                    name="password" 
+                    placeholder="Password"
+                    value={ password }
+                    onChange={ onInputChange }
+                />
+            </div>
+
+            {
+                (username === "gisbel") && <Message />
+            }
+        </>
+    )
+}
