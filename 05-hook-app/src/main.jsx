5d243bf068d9415a90f586c6094478e2f03471ba
GisbelT <gisbeltq@gmail.com>
2022-12-12 22:30:09 -0400

    useMemo

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -7,12 +7,13 @@ import { SimpleForm } from './02-useEffect/SimpleForm'
 import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 import { FocusScreen } from './04-useRef/FocusScreen'
 import { Layout } from './05-useLayoutEffect/Layout'
+import { MemoHook } from './06-memo/MemoHook'
 import { Memorize } from './06-memo/Memorize'
 // import { HooksApp } from './HooksApp'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Memorize />
+    <MemoHook />
   </React.StrictMode>
 )
