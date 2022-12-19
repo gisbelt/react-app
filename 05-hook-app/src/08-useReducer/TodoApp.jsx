e0eb6ca3a613e043e65ec3827675b711b25c4cfa
GisbelT <gisbeltq@gmail.com>
2022-12-19 17:07:45 -0400

    hook: useTodo

--- 05-hook-app/src/08-useReducer/TodoApp.jsx
+++ 05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -1,54 +1,14 @@
-import { useEffect, useReducer } from "react"
+import { useTodo } from "../hooks/useTodo";
 import { TodoAdd } from "./TodoAdd"
 import { TodoList } from "./TodoList"
-import { todoReducer } from "./todoReducer"
-
-const initialState = [];
-// Initializer: try to parse what is in the LocalStorage
-// If it is null, it fetches an empty array 
-const init = () => {
-    return JSON.parse( localStorage.getItem('Todos') ) || [];
-}
 
 export const TodoApp = () => {
 
-    const [state, dispatch] = useReducer(todoReducer, initialState, init)
-
-    // Save and read Todos in LocalStorage 
-    useEffect(() => {
-      localStorage.setItem('Todos', JSON.stringify( state ))
-    }, [state])
-    
-    // Add new Todo
-    const handleAddTodo = ( todo ) => {
-        const actionAdd = {
-            type: 'Add Todo',
-            payload: todo,
-        }
-        dispatch( actionAdd );
-    }
-
-    // Remove Todo
-    const handleDeleteTodo = ( id ) => {
-        const actionDelete = {
-            type: 'Remove Todo',
-            payload: id,
-        } 
-        dispatch( actionDelete );
-    }
-
-    // Mark as read
-    const handleToggleTodo = ( id ) => {
-        const actionToggle = {
-            type: 'Toggle Todo',
-            payload: id,
-        } 
-        dispatch( actionToggle );
-    }
+    const { state, stateCount, pendingTodosCount, handleAddTodo, handleDeleteTodo, handleToggleTodo } = useTodo();
 
     return (
         <>
-            <h1>Todo App 10, <small className="text-danger">to do: 2</small></h1>
+            <h1>Todo App {stateCount}, <small className="text-danger">to do: {pendingTodosCount}</small></h1>
             <hr />
             <div className="row">
                 <div className="col-7">
