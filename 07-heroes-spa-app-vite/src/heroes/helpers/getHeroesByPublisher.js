23a3b67ad9d4682aade73ad867ddbc9779f01b69
GisbelT <gisbeltq@gmail.com>
2023-02-08 21:41:27 -0400

    useNavigate(): replace - helper: get heroes by publisher

--- 07-heroes-spa-app-vite/src/heroes/helpers/getHeroesByPublisher.js
+++ 07-heroes-spa-app-vite/src/heroes/helpers/getHeroesByPublisher.js
@@ -8,5 +8,5 @@ export const getHeroesByPublisher = ( publisher ) => {
         throw new Error(`${ publisher } is not a valid publisher`);
     }
 
-    return heroes.filter( heroe => heroes.publisher === publisher )
+    return heroes.filter( heroe => heroe.publisher === publisher )
 }
