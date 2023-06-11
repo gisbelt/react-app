c8d32a9b1b570128b2264cf058fd9b61e6746aca
GisbelT <gisbeltq@gmail.com>
2023-06-10 21:04:28 -0400

    logout

--- 07-heroes-spa-app-vite/src/HeroesApp.jsx
+++ 07-heroes-spa-app-vite/src/HeroesApp.jsx
@@ -1,16 +1,7 @@
-import { useContext } from 'react';
-import { AuthContext } from './auth/context/AuthContext';
 import { AuthProvider } from './auth/context/AuthProvider'
 import { AppRoutes } from './routes/AppRoutes'
 
 export const HeroesApp = () => {
-
-    // const { user } = useContext( AuthContext )	
-
-	// useEffect(() => {
-    //     localStorage.setItem('user', JSON.stringify( user ));
-    // }, [user]);
-
     return (
         <AuthProvider >        
             <AppRoutes />
--- 07-heroes-spa-app-vite/src/auth/context/AuthProvider.jsx
+++ 07-heroes-spa-app-vite/src/auth/context/AuthProvider.jsx
@@ -22,18 +22,22 @@ export const AuthProvider = ({ children }) => {
 
 	const login = () => {
 		const user = { id: 'ABC', name: 'Gisbel Torres' }
-		const action = {
-			type: types.login,
-			payload: user
-		}
+		const action = { type: types.login, payload: user }
 		localStorage.setItem('user', JSON.stringify(user));
+		dispatch( action );
+	}
+
+	const logout = () => {
+		localStorage.removeItem('user');
+		const action = { type: types.logout }
 		dispatch( action );
 	}
 
 	return (
 		<AuthContext.Provider value={{
 			authState,
-			login 
+			login,
+			logout
 		}}>
 			{ children }
 		</AuthContext.Provider>
