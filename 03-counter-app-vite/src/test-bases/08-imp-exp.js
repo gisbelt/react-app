9e0326ac723b2097c7d6a97d4a5a08db8f3f209e
GisbelT <gisbeltq@gmail.com>
2023-06-10 23:48:21 -0400

    cleaning branch

--- 03-counter-app-vite/src/test-bases/08-imp-exp.js
+++
@@ -1,7 +0,0 @@
-import heroes from '../data/heroes';
-
-/*************FIND****************/
-export const getHeroeById = (id) => heroes.find( (heroe) => heroe.id === id );
-
-/*************FILTER****************/
-export const getHeroeByOwner = ( owner ) => heroes.filter ( (heroe) => heroe.owner === owner );
