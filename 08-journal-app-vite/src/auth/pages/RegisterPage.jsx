900fb4ea4709181f0e08b88c1f2d54c1721c3f2c
GisbelT <gisbeltq@gmail.com>
2023-06-15 13:35:04 -0400

    Register Design

--- 08-journal-app-vite/src/auth/pages/RegisterPage.jsx
+++ 08-journal-app-vite/src/auth/pages/RegisterPage.jsx
@@ -1,13 +1,22 @@
 import { Link as RouterLink } from 'react-router-dom'
 import { Button, Grid, Link, TextField, Typography } from '@mui/material'
-import { Google } from '@mui/icons-material'
 import { AuthLayout } from '../layout/AuthLayout'
 
 export const RegisterPage = () => {
 	return (
-		<AuthLayout title='Register'>
+		<AuthLayout title='Create an account'>
 			<form>
 				<Grid container >
+					<Grid item xs={ 12 } sx={{ mb: 2 }}>
+						<TextField 
+							label="Full Name" 
+							type="text" 
+							placeholder='Your full name'
+							fullWidth={ true }
+						>
+						</TextField>
+					</Grid>
+
 					<Grid item xs={ 12 } sx={{ mb: 2 }}>
 						<TextField 
 							label="Email Address" 
@@ -39,22 +48,17 @@ export const RegisterPage = () => {
 					</Grid>
 
 					<Grid container spacing={ 2 } sx={{ mb: 2 }}>
-						<Grid item xs={ 12 } sm={ 6 }>
+						<Grid item xs={ 12 }>
 							<Button variant="contained" fullWidth >
 								Sing up
-							</Button>
-						</Grid>
-						<Grid item xs={ 12 } sm={ 6 } >
-							<Button variant="contained" fullWidth >
-								<Google />
-								<Typography sx={{ ml: 1 }}>Google</Typography>
 							</Button>
 						</Grid>
 					</Grid>
 
 					<Grid container direction='row' justifyContent='end' >
-						<Link component={ RouterLink } color='inherit' to="/auth/register">
-							Create an account
+						<Typography sx={{ mr: 1 }}>Already have an account?</Typography>
+						<Link component={ RouterLink } color='inherit' to="/auth/login">
+							Login
 						</Link>
 					</Grid>
 				</Grid>
