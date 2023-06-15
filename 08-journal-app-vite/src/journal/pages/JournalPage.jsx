80283a61fb6d2edc4b53ba38da7d6e2a03e79e3e
GisbelT <gisbeltq@gmail.com>
2023-06-15 19:13:50 -0400

    Floating button

--- 08-journal-app-vite/src/journal/pages/JournalPage.jsx
+++ 08-journal-app-vite/src/journal/pages/JournalPage.jsx
@@ -5,10 +5,24 @@ export const JournalPage = () => {
 	return (
 		<JournalLayout>
 			{/* Nothing selected  */}
-			{/* <NothingSelectedView /> */}
+			<NothingSelectedView />
 
 			{/* Note View  */}
-			<NoteView />
+			{/* <NoteView /> */}
+
+			<IconButton
+				size='large'
+				sx={{
+					color: 'white',
+					backgroundColor: 'error.main',
+					':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+					position: 'fixed',
+					right: 50,
+					bottom: 50
+				}}
+			>
+					<AddOutlined sx={{ fontSize: 30 }}/>
+			</IconButton>
 		</JournalLayout>
 	)
 }
