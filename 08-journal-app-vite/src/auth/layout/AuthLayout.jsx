68a88b2d8af1138fb3b86603a9cde7271dde3752
GisbelT <gisbeltq@gmail.com>
2023-06-15 13:29:45 -0400

    Auth Layout

---
+++ 08-journal-app-vite/src/auth/layout/AuthLayout.jsx
@@ -0,0 +1,24 @@
+import { Grid, Typography } from '@mui/material'
+
+export const AuthLayout = ({ children, title='' }) => {
+	return (
+		<Grid
+			container
+			spacing={ 0 }
+			direction="column"
+			alignItems="center"
+			justifyContent="center"
+			sx={{ minHeight: '100vh', width: '100vw', backgroundColor: 'primary.main', padding: 4 }}
+		>
+			<Grid
+				item
+				className='box-shadow'
+				xs={ 3 } //On small screens, 3 positions
+				sx={{ width: {md: 450} ,backgroundColor: 'white', padding: 3, borderRadius: 2 }} //allows us to work with the styles property
+			>
+				<Typography variant='h5' sx={{ mb: 2 }}>{ title }</Typography>
+				{ children }
+			</Grid>
+		</Grid>
+	)
+}
