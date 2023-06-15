4840961d93c4c1ceb9089ad6fbe42944197d2d7e
GisbelT <gisbeltq@gmail.com>
2023-06-15 14:25:12 -0400

    Journal Layout

--- 08-journal-app-vite/src/journal/pages/JournalPage.jsx
+++ 08-journal-app-vite/src/journal/pages/JournalPage.jsx
@@ -1,11 +1,12 @@
 import { Typography } from '@mui/material'
 import { MailOutline } from '@mui/icons-material'
+import { JournalLayout } from '../layout/JournalLayout'
 
 export const JournalPage = () => {
 	return (
-		<>
-			<Typography variant='h1' >JournalPage</Typography>
+		<JournalLayout>
+			<Typography >Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias aspernatur corporis voluptatem dolor ex, repellendus, pariatur ad corrupti delectus, soluta eos. Enim beatae qui reprehenderit delectus eaque quidem officia deleniti!</Typography>
 			<MailOutline />
-		</>
+		</JournalLayout>
 	)
 }
