62f4a86ad67af446c687e5d97d7ddc677a5cb748
GisbelT <gisbeltq@gmail.com>
2023-06-15 18:45:14 -0400

    Views - Note View

--- 08-journal-app-vite/src/journal/pages/JournalPage.jsx
+++ 08-journal-app-vite/src/journal/pages/JournalPage.jsx
@@ -1,13 +1,14 @@
 import { JournalLayout } from '../layout/JournalLayout'
-import { NothingSelectedView } from '../views'
+import { NoteView, NothingSelectedView } from '../views'
 
 export const JournalPage = () => {
 	return (
 		<JournalLayout>
 			{/* Nothing selected  */}
-			<NothingSelectedView />
+			{/* <NothingSelectedView /> */}
 
 			{/* Note View  */}
+			<NoteView />
 		</JournalLayout>
 	)
 }
