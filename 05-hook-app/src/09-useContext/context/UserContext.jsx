3c9cce9c25748bb38e4f80f82efc102b43651f74
GisbelT <gisbeltq@gmail.com>
2023-06-15 19:19:34 -0400

    cleaning branch

--- 05-hook-app/src/09-useContext/context/UserContext.jsx
+++
@@ -1,4 +0,0 @@
-// createContext: It allows us to know what the information we are going to put there looks like and also to define the provider. 
-import { createContext } from "react";
-
-export const UserContext = createContext()
--- 07-heroes-spa-app-vite/src/auth/context/index.js
+++
@@ -1,3 +0,0 @@
-export * from './AuthContext'
-export * from './AuthContext'
-export * from './authReducer'
