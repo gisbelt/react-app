f4a2f1568fa6b35ea4ad2e1dea3e143d68ead854
GisbelT <gisbeltq@gmail.com>
2023-06-15 18:57:31 -0400

    Image Gallery

--- 08-journal-app-vite/src/journal/components/NavBar.jsx
+++ 08-journal-app-vite/src/journal/components/NavBar.jsx
@@ -51,7 +51,7 @@ export const NavBar = ({ drawerWidth }) => {
 
 					<Tooltip title="Open settings">
 						<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-							<Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+							<Avatar alt="Remy Sharp" src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e" />
 						</IconButton>
 					</Tooltip>
 					<Menu
