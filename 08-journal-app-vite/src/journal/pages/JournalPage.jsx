181303a1db1d5aa5ee42890cef0eba57c75c57c7
GisbelT <gisbeltq@gmail.com>
2023-06-15 19:12:42 -0400

    Revert "Floating button"
    This reverts commit a8a4e0e2adde61bee006b0e8db070674fc5224f1.

--- 08-journal-app-vite/src/journal/pages/JournalPage.jsx
+++ 08-journal-app-vite/src/journal/pages/JournalPage.jsx
@@ -1,5 +1,3 @@
-import { AddOutlined } from '@mui/icons-material'
-import { IconButton } from '@mui/material'
 import { JournalLayout } from '../layout/JournalLayout'
 import { NoteView, NothingSelectedView } from '../views'
 
@@ -7,24 +5,10 @@ export const JournalPage = () => {
 	return (
 		<JournalLayout>
 			{/* Nothing selected  */}
-			<NothingSelectedView />
+			{/* <NothingSelectedView /> */}
 
 			{/* Note View  */}
-			{/* <NoteView /> */}
-
-			<IconButton
-				size='large'
-				sx={{
-					color: 'white',
-					backgroundColor: 'error.main',
-					':hover': { backgroundColor: 'error.main', opacity: 0.9 },
-					position: 'fixed',
-					right: 50,
-					bottom: 50
-				}}
-			>
-				<AddOutlined sx={{ fontSize: 30 }}/>
-			</IconButton>
+			<NoteView />
 		</JournalLayout>
 	)
 }
