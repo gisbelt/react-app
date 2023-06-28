062602f88acb40fe2e36e8efc20d42ff3cda4ac0
GisbelT <gisbeltq@gmail.com>
2023-06-28 19:51:09 -0400

    Display pokemons in a paginated way

--- 09-toolkit-redux-vite/src/PokemonApp.jsx
+++ 09-toolkit-redux-vite/src/PokemonApp.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getPokemons } from './store/slices/pokemon'
 
 export const PokemonApp = () => {
 
 	const dispatch = useDispatch()
+	const { isLoading, pokemons = [], page } = useSelector(state => state.pokemons)
 
 	useEffect(() => {
 	  dispatch( getPokemons() );	
@@ -15,11 +16,21 @@ export const PokemonApp = () => {
 		<>
 			<h1>Pokemon App</h1>
 			<hr />
+
+			<span>Loading: { isLoading ? 'True' : 'False' }</span>
+			
 			<ul>
-				<li>Hola</li>
-				<li>Hola</li>
-				<li>Hola</li>
+				{ pokemons.map((pokemon, index) => (
+					<li key={ index } > {pokemon.name} </li>
+				))}
 			</ul>
+
+			<button
+				disabled = { isLoading }
+				onClick = { () => dispatch( getPokemons(page) ) }
+			>
+				Next Page
+			</button>
 		</>
 	)
 }
