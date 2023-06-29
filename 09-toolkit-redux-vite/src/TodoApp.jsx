b322260efe041467d19c977b98eff884cc63ca82
GisbelT <gisbeltq@gmail.com>
2023-06-28 22:15:35 -0400

    RTK Query custom hook and store

--- 09-toolkit-redux-vite/src/TodoApp.jsx
+++ 09-toolkit-redux-vite/src/TodoApp.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
+import { useGetTodosQuery } from './store/apis/todosApi'
 
 export const TodoApp = () => {
+
+	const { data: todos, error, isLoading } = useGetTodosQuery()
+
+	// console.log(data);
 	return (
 		<>
 			<h1>To-do - RTK Query</h1>
 			<hr />
-			<h4>IsLoading...</h4>
+			<h4>IsLoading: { isLoading ? 'True' : 'False' }</h4>
 
 			<pre>...</pre>
+
+			<ul>
+				{ todos.map((todo, index) => (
+					<li key={ index }>
+						<strong>{ todo.completed ? 'DONE ' : 'PENDING ' }</strong>
+						{ todo.title }
+					</li>
+				))}				
+			</ul>
 
 			<button>
 				Next To-do
--- 09-toolkit-redux-vite/src/store/store.js
+++ 09-toolkit-redux-vite/src/store/store.js
@@ -1,10 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { todosApi } from './apis/todosApi'
 import { counterSlice } from './slices/counter/counterSlices'
 import { pokemonSlice } from './slices/pokemon/pokemonSlice'
 
 export const store = configureStore({
  	reducer: {
 		counter: counterSlice.reducer,
-		pokemons: pokemonSlice.reducer
+		pokemons: pokemonSlice.reducer,
+		// Add the to-do api done in /src/store/apis with RTK query
+		[todosApi.reducerPath]: todosApi.reducer,
 	},
+	// Adding the api middleware enables caching, invalidation, polling,
+  	// and other useful features of `rtk-query`.
+  	middleware: (getDefaultMiddleware) =>
+    	getDefaultMiddleware().concat(todosApi.middleware),
+
 })
