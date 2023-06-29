47d1f9ba5c81186f7a6e562b6e615a07f6707c38
GisbelT <gisbeltq@gmail.com>
2023-06-28 21:41:04 -0400

    RTK Query

---
+++ 09-toolkit-redux-vite/src/TodoApp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const TodoApp = () => {
+	return (
+		<>
+			<h1>To-do - RTK Query</h1>
+			<hr />
+			<h4>IsLoading...</h4>
+
+			<pre>...</pre>
+
+			<button>
+				Next To-do
+			</button>
+		</>
+	)
+}
