0a0e945e2a1ac982f44aec00a35e220f49883ad4
GisbelT <gisbeltq@gmail.com>
2023-07-05 16:15:42 -0400

    Trigger authentication action (logout and login) and button locking with useMemo to store 'checking'

--- 08-journal-app-vite/src/auth/pages/LoginPage.jsx
+++ 08-journal-app-vite/src/auth/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
-import { useDispatch } from 'react-redux'
+import { useMemo } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link as RouterLink } from 'react-router-dom'
 import { Button, Grid, Link, TextField, Typography } from '@mui/material'
 import { Google } from '@mui/icons-material'
@@ -8,12 +9,14 @@ import { checkingAuthentication, startGoogleSignIn } from '../../store/auth/thun
 
 export const LoginPage = () => {
 
+	const { status } = useSelector(state => state.auth)
+	const dispatch = useDispatch()
 	const { email, password, onInputChange } = useForm({
 		email: 'gis@google.com',
 		password: '123456',
 	})
-	
-	const dispatch = useDispatch()
+
+	const isAuthenticating = useMemo( () => status === 'checking', [status] )
 
 	const onSubmit = e => {
 		e.preventDefault()
@@ -58,12 +61,18 @@ export const LoginPage = () => {
 
 					<Grid container spacing={ 2 } sx={{ mb: 2 }}>
 						<Grid item xs={ 12 } sm={ 6 }>
-							<Button type="submit" variant="contained" fullWidth >
+							<Button
+								disabled= { isAuthenticating } 
+								type="submit" variant="contained" fullWidth 
+							>
 								Login
 							</Button>
 						</Grid>
 						<Grid item xs={ 12 } sm={ 6 } >
-							<Button onClick={ onGoogleSignIn } variant="contained" fullWidth >
+							<Button 
+								disabled= { isAuthenticating }
+								onClick={ onGoogleSignIn } variant="contained" fullWidth 
+							>
 								<Google />
 								<Typography sx={{ ml: 1 }}>Google</Typography>
 							</Button>
