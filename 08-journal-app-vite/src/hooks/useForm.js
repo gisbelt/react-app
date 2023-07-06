29a3ccb0aaf3159e8544b13ab4ac6d26fbb0ba0b
GisbelT <gisbeltq@gmail.com>
2023-07-05 22:14:48 -0400

    RegisterPage form validation - useForm

--- 08-journal-app-vite/src/hooks/useForm.js
+++ 08-journal-app-vite/src/hooks/useForm.js
@@ -1,10 +1,36 @@
-import { useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 
-export const useForm = ( initialForm = {} ) => {
+export const useForm = ( initialForm = {}, formValidations = {} ) => {
     
     // initialForm: which by default will be an object. 
     const [formState, setFormState] = useState( initialForm )
+    const [formValidation, setFormValidation] = useState({})
     const inputRef = useRef();
+
+    const createValidators = () => {
+        const formCheckedValues = {};
+        // We sweep each key from formValidations, each property name
+        for (const formField of Object.keys( formValidations )) {
+            // the function and error message that each property of the formValidations has in the formField position
+            const [ fn, errorMessage ] = formValidations[formField];
+            // Remember that the function (fn) must be given the value of each input as parameter 
+            formCheckedValues[`${ formField }Valid`] = fn( formState[formField] ) ? null : errorMessage;
+        }
+        setFormValidation(formCheckedValues);
+    }
+
+    useEffect(() => {
+        createValidators();       
+    }, [formState])
+
+    // We will store the value y that value should change only if the formValidation changes. 
+    const isFormValid = useMemo( () => {
+        for (const formValue of Object.keys( formValidation )) {
+            // We return false so that the for loop does not continue executing. 
+            if( formValidation[formValue] !== null ) return false;
+        }
+        return true;
+    }, [formValidation])
 
     //change input
     const onInputChange = ({ target }) => {
@@ -21,12 +47,15 @@ export const useForm = ( initialForm = {} ) => {
         setFormState( initialForm )
         inputRef.current.focus();
     }
-
+    
     return {
         ...formState, //part of the object that the useForm is returning so that it can be unstructured.
         formState,
         onInputChange,
         onResetForm,
         inputRef,
+
+        ...formValidation,
+        isFormValid
     }
 }
