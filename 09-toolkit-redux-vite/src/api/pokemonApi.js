2051c9bac7520b24beea174aa7f5822121cd4436
GisbelT <gisbeltq@gmail.com>
2023-07-09 20:24:06 -0400

    cleaning branch

--- 09-toolkit-redux-vite/src/api/pokemonApi.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-
-// allows us to create a standard configuration that will help us to avoid repeating code over and over again. 
-export const pokemonApi = axios.create({
-	baseURL: 'https://pokeapi.co/api/v2'
-});
