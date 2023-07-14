14a98614fc4ad910b5e967b25e0a699498cc3881
GisbelT <gisbeltq@gmail.com>
2023-07-13 20:21:00 -0400

    Create new note

--- 08-journal-app-vite/src/store/journal/thunks.js
+++ 08-journal-app-vite/src/store/journal/thunks.js
@@ -1,12 +1,21 @@
+import { collection, doc, setDoc } from "firebase/firestore/lite";
+import { FirebaseDB } from "../../firebase/config";
+
 export const startNewNote = () => {
-	return async ( dispatch ) => {
-		// uid
+	return async ( dispatch, getState ) => {
+
+		const { uid } = getState().auth;
+
 		const newNote = {
 			title: '',
 			body: '',
 			date: new Date().getTime(),
 		}
 
+		// Create the reference to the document 
+		const newDoc = doc( collection( FirebaseDB, `${uid}/journal/notes` ))
+		// Insert new note to document 
+		const setDocResp = await setDoc( newDoc, newNote )
 		// dispatch( newNote )
 		// dispatch( activeNote )
 	}
