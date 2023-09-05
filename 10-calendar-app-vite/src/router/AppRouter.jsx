f1796e5265185389925b177966d83398e9219b71
GisbelT <gisbeltq@gmail.com>
2023-09-04 20:59:56 -0400

    Calendar App - React Router Dom

---
+++ 10-calendar-app-vite/src/router/AppRouter.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { LoginPage } from '../auth'
+import { CalendarPage } from '../calendar'
+
+export const AppRouter = () => {
+	const authStatus = 'not-authenticated' //authenticated
+	
+	return (
+		<Routes>
+			{
+				(authStatus === 'not-authenticated')
+				? <Route path='/auth/*' element={ <LoginPage /> } />
+				: <Route path='/*' element={ <CalendarPage /> } />
+			}
+			
+			{/* any path redirects to login  */}
+			<Route path='/*' element={ <Navigate to='/auth/login' /> } />
+		</Routes>
+	)
+}
