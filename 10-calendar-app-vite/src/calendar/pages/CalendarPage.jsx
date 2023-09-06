c8f273807d49e0307baeb64256187b563b468da7
GisbelT <gisbeltq@gmail.com>
2023-09-06 18:28:18 -0400

    Add: helper localizer and getMessageES - eventStyleGetter

--- 10-calendar-app-vite/src/calendar/pages/CalendarPage.jsx
+++ 10-calendar-app-vite/src/calendar/pages/CalendarPage.jsx
@@ -1,20 +1,8 @@
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import { Calendar } from 'react-big-calendar'
+import { localizer, getMessagesES } from '../../helpers'
+import { addHours } from 'date-fns'
 import { Navbar } from '../'
-import { addHours, format, parse, startOfWeek, getDay } from 'date-fns'
-import enUS from 'date-fns/locale/en-US'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
-
-const locales = {
-	'en-US': enUS,
-}
-
-const localizer = dateFnsLocalizer({
-	format,
-	parse,
-	startOfWeek,
-	getDay,
-	locales,
-})
 
 const events = [{
 	title: 'Birdthday',
@@ -29,16 +17,31 @@ const events = [{
 }]
 
 export const CalendarPage = () => {
+
+	const eventStyleGetter = ( event, start, end, isSelected ) => {
+		const style = {
+			backgroundColor: '#347C99',
+			borderRadius: '0px',
+			opacity: 0.8,
+			color: 'white',
+		}
+		
+		return { style }
+	}
+	
 	return (
 		<>
 			<Navbar />
 
 			<Calendar
+				culture='es'
 				localizer={localizer}
 				events={events}
 				startAccessor="start"
 				endAccessor="end"
 				style={{ height: 'calc(100vh - 56px - 1.5rem)' }}
+				messages={getMessagesES()}
+				eventPropGetter={ eventStyleGetter }
 			/>
 		</>
 	)
