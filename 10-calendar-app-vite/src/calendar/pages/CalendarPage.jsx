af643fcdbe88d48ffd74ae20a0a6fcc6fdf35662
GisbelT <gisbeltq@gmail.com>
2023-09-14 22:44:01 -0400

    Add: floating action button

--- 10-calendar-app-vite/src/calendar/pages/CalendarPage.jsx
+++ 10-calendar-app-vite/src/calendar/pages/CalendarPage.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { Calendar } from 'react-big-calendar'
 import { localizer, getMessagesES } from '../../helpers'
-import { CalendarEventBox, CalendarModal, Navbar } from '../'
+import { CalendarEventBox, CalendarModal, FabAddNew, Navbar } from '../'
 import { useUiStore, useCalendarStore } from '../../hooks'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 
@@ -58,6 +58,7 @@ export const CalendarPage = () => {
 			/>
 
 			<CalendarModal />
+			<FabAddNew />
 		</>
 	)
 }
